5d92336c3695521a26ee98d73184367e25a9a9f5
Chris Griffin <chrisgriffin2001@icloud.com>
2023-02-18 15:45:42 +0000

    files updated and and read.me updated

--- HTML/assets/script.js
+++ HTML/assets/script.js
@@ -1,55 +1,51 @@
+const apiText = document.getElementById('api-text')
+const generateButton = document.getElementById('generate-button')
+const apiUrl = 'https://v2.jokeapi.dev/joke/Any?safe-mode'
+
 
 function fetchQuote(){
 fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    apiText.innerHTML = data.text;
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.type === 'single') {
+        apiText.innerHTML = data.joke
+    } else {
+        apiText.innerHTML = data.setup + ' ' + data.delivery
+    }
+    
   })
-  .catch(error => {
-    console.error("Error fetching data from API:", error);
+  .catch((error) => {
+    console.log(error)
+    apiText.innerHTML = 'Oops! Something went wrong.'
   });
 }
 
 generateButton.addEventListener("click", fetchQuote);
 
 // Fetch initial quote on page load
-fetchQuote();
+fetchQuote()
 
+console.log(`User Agent: ${navigator.userAgent}`)
 
-
-  console.log(`User Agent: ${navigator.userAgent}`);
+generateButton.addEventListener('click', function () {
 
   fetch(apiUrl)
-  .then(response => {
-    console.log(response);
-    return response.json();
-  })
-  .then(data => {
-    apiText.innerHTML = data.text;
+  .then(response => response.json ())
+  .then((data) => {
+    if (data.type === 'single') {
+        apiText.innerHTML = data.joke
+    } else {
+        apiText.innerHTML = data.setup + ' ' + data.delivery
+    }
   })
   .catch(error => {
-    console.error("Error fetching data from API:", error);
+    console.log(error)
+    return response.json();
+    apiText.innerHTML = 'Oops! Something went wrong.'
+  
+  })
 
-  });
-  const apiText = document.getElementById("api-text");
-  const generateButton = document.getElementById("generate-button");
-  const apiUrl = "https://v2.jokeapi.dev/joke/Any?safe-mode";
-  
-  generateButton.addEventListener("click", function() {
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        if (data.type === "single") {
-          apiText.innerHTML = data.joke;
-        } else {
-          apiText.innerHTML = data.setup + " " + data.delivery;
-        }
-      })
-      .catch(error => {
-        console.log(error);
-        apiText.innerHTML = "Oops! Something went wrong.";
-      });
-  });
-  
+})
+
 
 
--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 # This-is-my-first-solo-project-project-1
 Bootcamp Project 1
 
-Project Unfinished 
 
-This is my random quote generator, although the API url is actually a joke link i will be changing that later as i have a few issues at the moment and some errors.
 
-thanks
+*Description*
+
+This is my random joke generator, its a simple web application then when you press the ( Generate ) button it gives you a random joke. As a side not if this application seems a bit rushed that’s because it is, I’ve been chasing my tail a bit on this course with one thing and another.
+
+Thanks and enjoy !!!
+
+*Technologies used*
+
+* HTML5: The code is written in HTML5, which is the latest version of the HTML markup language used to structure content on the web.
+* Bootstrap: The code is using Bootstrap, a popular CSS framework that provides pre-built CSS styles and JavaScript components to help build responsive and mobile-first websites.
+* CSS: The code is using CSS (Cascading Style Sheets) to apply custom styles to HTML elements.
+* JavaScript: The code includes JavaScript code to fetch a random joke from an external API and display it on the page, as well as to handle user interactions with the page (such as clicking the "Generate" button).
+* jQuery: The code is using jQuery, a JavaScript library, to manipulate the DOM (Document Object Model) and make it easier to interact with HTML elements in the code.
+* Popper.js: The code is using Popper.js, a JavaScript library, to manage popovers, tooltips, and dropdowns in the Bootstrap framework.
+* External APIs: The code is using an external API, specifically the JokeAPI, to fetch a random joke to display on the page.
+
+
+
+
+￼
+
+Deployed Link:
+
+https://chrisgriffin2001.github.io/This-is-my-first-solo-project-project-1/
   
